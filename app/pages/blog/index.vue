f7c290fce2871d3f6086e5d9ec2d8b3ef1536af6
Refired <alexshepherdno1@gmail.com>
2026-09-21 01:35:19 +0100

    feat: Simple styling for recent blog list

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -12,12 +12,12 @@
 
 <template>
     <NuxtLink to="/">Home</NuxtLink>
-    <h2 id="blogCount">I have written {{ blogCount }} blogs.</h2>
+    <h3 id="blogCount">I have written {{ blogCount }} blogs.</h3>
     <div id="recentBlogs">
         <h2>Recent Blogs</h2>
         <ul>
             <li v-for="blog in recentBlogs">
-                <NuxtLink :to="'blog/'+blog.slug">{{ blog.title }} -- {{ blog.dateCreated }}</NuxtLink>
+                <NuxtLink :to="'blog/'+blog.slug">{{ blog.title }}</NuxtLink>
             </li>
         </ul>
     </div>
@@ -27,6 +27,43 @@
     #blogCount {
         position: absolute;
         top:0px;
-        right:10px;
+        right:20px;
     }
+
+    #recentBlogs {
+        width:10rem;
+
+    }
+
+    #recentBlogs > h2 {
+        background-color:wheat;
+        border:goldenrod 2px double;
+        border-bottom:0;
+        font-variant:small-caps;
+        text-align:center;
+        margin:0;
+        transition:all 1s;
+    }
+
+    #recentBlogs > ul {
+        background-color:wheat;
+        border:goldenrod 2px double;
+        margin:0;
+        list-style:none;
+        transition:all 1s;
+        text-align:center;
+        padding:0;
+    }
+
+    #recentBlogs > ul > li > a{
+        color: black;
+        transition: 0.2s all;
+    }
+
+    #recentBlogs > ul > li > a:hover {
+        color: white;
+        -webkit-text-stroke: 1px black;
+        color: transparent; /* optional: makes it hollow/outlined-only */
+    }
+
 </style>
